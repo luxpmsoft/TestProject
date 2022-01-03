ea1609953b619a7286161623950116172f1a1a0f
Asad Zaman <hmasad09@gmail.com>
2022-01-04 00:16:06 +0900

    Status code

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get('/hello',(req,res)=>{
-    return res.send({
+    return res.status(200).send({
         "message" : "Hello World"
     });
 })
 
 const PORT  = 3000;
-app.listen(PORT,(req,res)=>{
+app.listen(PORT,()=>{
     console.log("App is running on PORT:",PORT)
 })
